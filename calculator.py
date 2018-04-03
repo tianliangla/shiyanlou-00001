97850c8e51a9dc0ce641f83e779951efac273690
tianliangla <18973518158@189.cn>
2018-04-03 12:39:18 +0800

    中文化注释

--- calculator.py
+++ calculator.py
@@ -13,21 +13,24 @@ try:
 except:
     print("Parameter Error")
 
-#gongzi
+#工资 gongzi
 #gongzi = int(sys.argv[1])
 
-#baoxianfei set 0
+#保险费初始化0
 baoxian = 0
 
-#qizhengdian set 3500
+#起征点初始化为 3500
 qzd = 3500
 
-#ying_nasui_suode_e
+#应纳税所得额sde = 工资 － 各项保险费 －起征点
 sde = gongzi - baoxian - qzd
 
-#ying_nasui
+#应纳税俄yns为浮点数
+#应纳税俄＝应纳税所得额X税率－速算扣除数
 yns = 0.00
 
+
+#税率值不能直接用百分数，要转成浮点小数才可正常计算
 if sde <= 1500 :
     yns = sde * 0.03 - 0
 elif sde > 1500 and sde <= 4500:
